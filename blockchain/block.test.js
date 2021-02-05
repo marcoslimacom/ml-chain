b4999a6c05f2a6edcdf96960fe08f50cf072b9d0
Marcos Lima <marcoslima@marcoslima.com>
2021-02-04 19:56:31 -0800

    Updated logic of hash with more difficult level

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,4 +1,5 @@
 const Block = require("./block");
+const { DIFFICULTY } = require("../config");
 
 describe("Block", () => {
   let data, lastBlock, block;
@@ -15,5 +16,9 @@ describe("Block", () => {
 
   it("sets the `lastHash` to match the math of the last block", () => {
     expect(block.lastHash).toEqual(lastBlock.hash);
+  });
+
+  it("generates a hash that matches the difficult", () => {
+    expect(block.hash.substring(0, DIFFICULTY)).toEqual("0".repeat(DIFFICULTY));
   });
 });
